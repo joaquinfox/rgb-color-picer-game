61c5c0ac43d3da1d7c0ea21fca2d727d8e340456
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-07-02 14:13:20 -0700

    reset function, win event handler

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const gameLength = 6;
 const colorSquares = document.querySelectorAll('.color_square');
-const colorArray = makeColorArray(gameLength);
-const targetColor = pickTargetColor();
-const targetColorDisplay = document.querySelector('#target_color_display');
+let colorArray = makeColorArray(gameLength);
+let targetColor = pickTargetColor();
+let targetColorDisplay = document.querySelector('#target_color_display');
+let userMessage = document.querySelector('#user_message');
+const resetButton = document.querySelector('#reset');
 
-// reset();
+reset();
 
-for (let i = 0; i < colorSquares.length; i++) {
-  colorSquares[i].style.backgroundColor = colorArray[i];
+resetButton.addEventListener('click', function () {
+  reset();
+});
+
+function reset() {
+  colorArray = makeColorArray(gameLength);
+  targetColor = pickTargetColor();
+  for (let i = 0; i < colorSquares.length; i++) {
+    colorSquares[i].style.backgroundColor = colorArray[i];
+  }
+  targetColorDisplay.innerText = targetColor;
 }
-targetColorDisplay.innerText = targetColor;
+
+// GAME LOGIC
+for (let i = 0; i < colorSquares.length; i++) {
+  colorSquares[i].addEventListener('click', () => {
+    if (colorSquares[i].style.backgroundColor === targetColor) {
+      userMessage.innerText = 'You got it!';
+      gameWin();
+    } else {
+      userMessage.innerText = 'Try again';
+    }
+  });
+}
+
+// Handle event gameWin
+function gameWin() {}
 
 // COLOR RELATED
 function rand(n) {
