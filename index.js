0b78ab53473ac3ad9abfedd51c17039eddba3eb6
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-07-02 13:27:32 -0700

    basic set up, color array rendered, target color rendered

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let gameLength = 6;
-let colorSquares = document.querySelectorAll('.color_squares');
-let colorArray = makeColorArray(gameLength);
+const gameLength = 6;
+const colorSquares = document.querySelectorAll('.color_square');
+const colorArray = makeColorArray(gameLength);
+const targetColor = pickTargetColor();
+const targetColorDisplay = document.querySelector('#target_color_display');
+
+// reset();
+
+for (let i = 0; i < colorSquares.length; i++) {
+  colorSquares[i].style.backgroundColor = colorArray[i];
+}
+targetColorDisplay.innerText = targetColor;
 
 // COLOR RELATED
 function rand(n) {
@@ -11,13 +20,17 @@ function makeColor() {
   let r = rand(256);
   let b = rand(256);
   let g = rand(256);
-  return `rgb(${r}, ${g} ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 function makeColorArray(n) {
   let array = Array(n);
   for (let i = 0; i < array.length; i++) {
-    array.push(makeColor());
+    array[i] = makeColor();
   }
   return array;
+}
+
+function pickTargetColor() {
+  return colorArray[rand(gameLength)];
 }
